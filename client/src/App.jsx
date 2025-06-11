6acbd3efe3889927fffcd108689184c0801ec5f9
Md Rakibur Rahman <mdrakibur179@gmail.com>
2025-06-11 14:38:20 +0600

    added pages and router

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+    </Routes>
+  );
+};
+export default App;
