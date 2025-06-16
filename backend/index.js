a9728535957cfdb2d819efad9008025d215c8f19
Md Rakibur Rahman <mdrakibur179@gmail.com>
2025-06-16 14:38:55 +0600

    connected to the database

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 
 const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
 app.listen(3000, () => {
+  connectDB();
   console.log("App is listening");
 });
