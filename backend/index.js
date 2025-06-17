f82b923f3c73491c4e70e2ad094fb4d4a1ac27c8
mdrakibur179 <mdrakibur179@gmail.com>
2025-06-17 09:29:32 +0600

    added test router

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+app.use("/api/user", userRoutes);
 
 app.listen(3000, () => {
   connectDB();
